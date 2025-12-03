a16544a80406087029707d54110e804bbd81632d
Paul Reece <paulreece42@gmail.com>
2025-12-03 12:16:44 -0500

    more testing and experimenting, nesting functions etc

--- main.cpp
+++ main.cpp
@@ -5,22 +5,21 @@ using namespace std;
 int parse_input(string myinput) {
     // parse input, return number base based off formatting
     // currently only does base-10 / decimal and base-16 / hex
-    cout << "in parse_input function, myinput is: " << myinput << endl;
     return 10;
 }
 
 string convert_to_binary(string myinput, int base) {
     // converts from base-x to binary, returned as string output
-    cout << "in convert_to_binary function, myinput is: " << myinput << " and base is: " << base << endl;
-    return myinput;
+    string myoutput;
+    myoutput = "in convert_to_binary function, myinput is: " + myinput + " and base is: " + to_string(base);
+    return myoutput;
 }
 
 int main() {
     string myinput;
     cout << "Enter a number in hex or decimal: ";
     cin >> myinput;
-    parse_input(myinput);
-    convert_to_binary(myinput, 10);
+    cout << convert_to_binary(myinput, parse_input(myinput)) << endl;
     
     return 0;
 }
