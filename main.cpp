ba9a2d3d4fa6bfccadd47b131bd6f27b23ef8246
Paul Reece <paulreece42@gmail.com>
2025-12-03 12:22:14 -0500

    c++ finally got regex yayyyy

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <regex> // holy cow guys! C++11 got regex! Finally! :-D
 using namespace std;
 
 
 int parse_input(string myinput) {
     // parse input, return number base based off formatting
     // currently only does base-10 / decimal and base-16 / hex
-    return 10;
+    regex pattern("^0x");
+    smatch match;
+    if (regex_search(myinput, match, pattern)) {
+        return 16;
+    }
+    else {
+        return 10;
+    }
 }
 
 string convert_to_binary(string myinput, int base) {
